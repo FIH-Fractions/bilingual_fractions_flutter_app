163c12caa99bb7ad9c76f9b023467102f29f3a8e
Neha Sawant <109304726+nehasawant4@users.noreply.github.com>
2024-02-18 13:13:29 -0800

    Tab length

--- lib/profile/progress.dart
+++ lib/profile/progress.dart
@@ -13,7 +13,7 @@ class _progressState extends State<progress> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3, // Number of tabs
+      length: 2, // Number of tabs
       child: Scaffold(
         appBar: AppBar(
           actions: [
