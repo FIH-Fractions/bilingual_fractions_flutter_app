7a6827199b309bef174093a60ace98ef650520f8
Neha Sawant <109304726+nehasawant4@users.noreply.github.com>
2024-02-19 02:44:40 -0800

    main

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
-  int _selectedIndex = 3;
+  int _selectedIndex = 0;
   static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
     home(),
