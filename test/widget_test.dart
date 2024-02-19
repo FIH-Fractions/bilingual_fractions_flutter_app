68bb84088f6b7651af637d717db6c45b6449eace
Neha Sawant <109304726+nehasawant4@users.noreply.github.com>
2024-02-18 20:38:00 -0800

    updated filenames

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:Bilingual_Fractions_Flutter_App/profile/progress.dart';
+import 'package:bilingual_fractions_flutter_app/profile/progress.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
